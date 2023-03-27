44659e2a2d7d8ac2f4ddb14c77624fe95fa26f35
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-03-27 15:07:41 -0400

    Refactored Code Spacing From 4 To 2

--- main.py
+++ main.py
@@ -9,116 +9,117 @@ from utils.cursor import Cursor
 from utils.input import print_dummy_board, clear, validate
 from utils.board import print_board, update_board
 
+def algorithm(stdscr, cursor: Cursor, board: List[List[int]], krow: int, kcol: int) -> None:
+  # WARNSDORFF'S ALGORITHM IMPLEMENTATION
+  # Directions The Knight Can Move On The Chessboard
+  dx: List[int] = [1, 2, 2, 1, -1, -2, -2, -1]
+  dy: List[int] = [-2, -1, 1, 2, 2, 1, -1, -2]
 
-def algorithm(stdscr, cursor: Cursor, board: List[List[int]], krow: int,
-              kcol: int) -> None:
-    '''
-    warnsdorff's algorithm implementation
-    '''
-    # directions the Knight can move on the chessboard
-    dx: List[int] = [1, 2, 2, 1, -1, -2, -2, -1]
-    dy: List[int] = [-2, -1, 1, 2, 2, 1, -1, -2]
+  for _ in range(64):
+    board[krow][kcol] = 1
+    # Priority Queue Of Avialable Neighbors
+    pq: List[Tuple[int, int]] = []
 
-    for _ in range(64):
-        board[krow][kcol] = 1
-        pq: List[Tuple[int, int]] = []  # priority queue of avialable neighbors
+    # Degree Of Neighbors
+    for i in range(8):
+      nrow: int = krow + dx[i]
+      ncol: int = kcol + dy[i]
 
-        # degree of neighbors
-        for i in range(8):
-            nrow: int = krow + dx[i]
-            ncol: int = kcol + dy[i]
+      if 0 <= nrow <= 7 and 0 <= ncol <= 7 and board[nrow][ncol] == 0:
+        # Count The Available Neighbors Of The Neighbor
+        count = 0
+        for j in range(8):
+          nnrow: int = nrow + dx[j]
+          nncol: int = ncol + dy[j]
 
-            if 0 <= nrow <= 7 and 0 <= ncol <= 7 and board[nrow][ncol] == 0:
-                # count the available neighbors of the neighbor
-                count = 0
-                for j in range(8):
-                    nnrow: int = nrow + dx[j]
-                    nncol: int = ncol + dy[j]
-
-                    if 0 <= nnrow <= 7 and 0 <= nncol <= 7 and board[nnrow][
-                            nncol] == 0:
-                        count += 1
-                heappush(pq, (count, i))
-
-        if len(pq) > 0:
-            (p, m) = heappop(pq) # knight's next position
-            krow += dx[m]
-            kcol += dy[m]
-            board[krow][kcol] = 2
-            update_board(stdscr, cursor, board)
-        else:
-            board[krow][kcol] = 1
-            update_board(stdscr, cursor, board)            
-
+          if 0 <= nnrow <= 7 and 0 <= nncol <= 7 and board[nnrow][nncol] == 0:
+            count += 1
+        heappush(pq, (count, i))
+    
+    if len(pq) > 0:
+      # Knight's Next Position
+      (p, m) = heappop(pq)
+      krow += dx[m]
+      kcol += dy[m]
+      board[krow][kcol] = 2
+      update_board(stdscr, cursor, board)
+    else:
+      board[krow][kcol] = 1
+      update_board(stdscr, cursor, board) 
 
 def visualize(stdscr, pos: Tuple[int, int]) -> None:
-    # clear the window
-    stdscr.clear()
+  # Clear The Window
+  stdscr.clear()
 
-    # hide the cursor
-    curses.curs_set(0)
+  # Hide The Cursor
+  curses.curs_set(0)
 
-    # colors
-    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # boundry
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)  # visited cell
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)  # knight's cell
-    curses.init_pair(4, curses.COLOR_WHITE,
-                     curses.COLOR_BLACK)  # unvisited cell
-    curses.init_pair(5, curses.COLOR_MAGENTA,
-                     curses.COLOR_BLACK)  # progress bar
+  # COLORS
+  # Boundry
+  curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
-    # get max x and y co-ordinates of the window
-    max_y, max_x = stdscr.getmaxyx()
+  # Visitied Cell
+  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
-    # 8 x 8 chess board
-    board: List[List[int]] = [[0] * 8 for _ in range(8)]
+  # Knight's Cell
+  curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    # initialize the cursor
-    cursor: Cursor = Cursor(max_x, max_y)
+  # Unvisited Cell
+  curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    # place the knight on the board
-    board[pos[0]][pos[1]] = 2
+  # Progress Bar
+  curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+  # END OF COLORS
 
-    # print the chess board
-    print_board(stdscr, cursor, board, sleep_value=2.0, initialize=True)
+  # Get Max X And Y Co-ordinates Of The Window
+  max_y, max_x = stdscr.getmaxyx()
 
-    # start the warnsdorff algorithm
-    algorithm(stdscr, cursor, board, pos[0], pos[1])
+  # 8 x 8 Chess Board
+  board: List[List[int]] = [[0] * 8 for _ in range(8)]
 
-    sleep(5.0)
+  # Initialize The Cursor
+  cursor: Cursor = Cursor(max_x, max_y)
 
+  # Place The Knight On The Board
+  board[pos[0]][pos[1]] = 2
+
+  # Print The Chess Board
+  print_board(stdscr, cursor, board, sleep_value=2.0, initialize=True)
+
+  # Start The Warnsdorff Algorithm
+  algorithm(stdscr, cursor, board, pos[0], pos[1])
+
+  sleep(5.0)
 
 def main() -> None:
-    # initialize colorama
-    init(autoreset=True)
+  # Initialize Colorama
+  init(autoreset=True)
 
-    while True:
-        clear()
-
-        # print dummy chess board to help user decide knight's position
-        print_dummy_board()
-
-        pos = input('knight\'s position (row, col): ')
-
-        # check user's input
-        if validate(pos):
-            break
-        else:
-            print(Fore.RED + 'Invalid, Try Again.')
-            sleep(1)
-
-    # start visualization
-    curses.wrapper(visualize, tuple(map(int, pos.split(','))))
-
+  while True:
     clear()
 
+    # Print Dummy Chess Board To Help User Decide Knight's Position
+    print_dummy_board()
+
+    pos = input('knight\'s position (row, col): ')
+
+    # Check User's Input
+    if validate(pos):
+      break
+    else:
+      print(Fore.RED + 'Invalid, Try Again.')
+      sleep(1)
+  
+  # Start Visualization
+  curses.wrapper(visualize, tuple(map(int, pos.split(','))))
+
+  clear()
 
 if __name__ == '__main__':
-    # Get the size 
-    # of the terminal 
-    size = os.get_terminal_size() 
-    
-    if size[0] < 55 or size[1] < 25:
-        sys.exit('Maximize your terminal window. (Req: column=55, lines=25)')
-    
-    main()
+  # Get The Size Of The Terminal
+  size = os.get_terminal_size()
+
+  if size[0] < 55 or size[1] < 25:
+    sys.exit('Maximize your terminal window. (Req: column=55, lines=25)')
+
+  main()
--- utils/board.py
+++ utils/board.py
@@ -3,124 +3,92 @@ from .cursor import Cursor
 from typing import List
 from time import sleep
 
-
 def horizontal_line(stdscr, cursor: Cursor) -> None:
-    '''
-    horizontal boundry line
-    '''
-    cursor.set_x(1)
-    stdscr.addstr(cursor.get_y(), cursor.get_x(), '-' * 29,
-                  curses.color_pair(1))
-    cursor.set_y(1)
-
+  # Horizontal Boundry Line
+  cursor.set_x(1)
+  stdscr.addstr(cursor.get_y(), cursor.get_x(), '-' * 29, curses.color_pair(1))
+  cursor.set_y(1)
 
 def vertical_line(stdscr, cursor: Cursor, side: str) -> None:
-    '''
-    vertical boundry line
-    '''
-    if side == 'L':
-        stdscr.addstr(cursor.get_y(), cursor.get_x(), '|',
-                      curses.color_pair(1))
-        cursor.set_x(1)
-    elif side == 'R':
-        stdscr.addstr(cursor.get_y(), cursor.get_x(), '|',
-                      curses.color_pair(1))
-        cursor.set_y(1)
-    else:
-        raise ValueError('invalid value for argument \'side\'')
+  # Vertical Boundry Line
+  if side == 'L':
+    stdscr.addstr(cursor.get_y(), cursor.get_x(), '|', curses.color_pair(1))
+    cursor.set_x(1)
+  elif side == 'R':
+    stdscr.addstr(cursor.get_y(), cursor.get_x(), '|', curses.color_pair(1))
+    cursor.set_y(1)
+  else:
+    raise ValueError('invalid value for argument \'side\'')
 
 
 def get_progress(board) -> str:
-    '''
-    returns the progress of the algorithm
-    '''
-    visited_cell_count = 0
-    total_cell_count = 64
+  # Return The Progress Of The Algorithm
+  visited_cell_count = 0
+  total_cell_count = 64
 
-    for row in range(8):
-        visited_cell_count += board[row].count(1)
+  for row in range(8):
+    visited_cell_count += board[row].count(1)
+  
+  progress = (visited_cell_count / total_cell_count) * 100
 
-    progress = (visited_cell_count / total_cell_count) * 100
-
-    return f'{progress}%'
-
+  return f'{progress}%'
 
 def update_board(stdscr, cursor: Cursor, board: List[List[int]]) -> None:
-    '''
-    paint the updated board on the window
-    '''
-    cursor.reset_x()
-    cursor.reset_y()
-    print_board(stdscr, cursor, board, progress=True)
-
+  # Paint The Updated Board On The Window
+  cursor.reset_x()
+  cursor.reset_y()
+  print_board(stdscr, cursor, board, progress=True)
 
 def print_progress_bar(stdscr, cursor: Cursor, board: List[List[int]]) -> None:
-    '''
-    paint the progress bar on the window
-    '''
+  # Paint The Progress Bar On The Window
+  cursor.set_x(-cursor.get_x())
+  cursor.set_y(-cursor.get_y())
+  stdscr.addstr(cursor.get_y(), cursor.get_x(), 'completed: ')
+  stdscr.addstr(cursor.get_y(), cursor.get_x() + 11, ' ' * cursor.max_x)
+  stdscr.addstr(cursor.get_y(), cursor.get_x() + 11, get_progress(board), curses.color_pair(5))
+
+def print_board(stdscr, cursor: Cursor, board: List[List[int]], progress: bool = False, sleep_value: float = 0.5, initialize: bool = False) -> None:
+  # Paint The Board On The Window
+  horizontal_line(stdscr, cursor)
+  for row in range(8):
+    cursor.reset_x()
+
+    # Print Empty Line
+    if 0 < row <= 7:
+      stdscr.addstr(cursor.get_y(), cursor.get_x(), '|' + ' ' * 29 + '|', curses.color_pair(1))
+      cursor.set_y(1)
+    
+    # Print Left Border
+    vertical_line(stdscr, cursor, side='L')
+
+    for col in range(8):
+      # Cell Is Visited
+      if board[row][col] == 1:
+        stdscr.addstr(cursor.get_y(), cursor.get_x(), str(board[row][col]), curses.color_pair(2))
+      # Knight's Cell
+      elif board[row][col] == 2:
+        stdscr.addstr(cursor.get_y(), cursor.get_x(), str(board[row][col]), curses.color_pair(3))
+      # Cell Is Unvisited
+      else:
+        stdscr.addstr(cursor.get_y(), cursor.get_x(), str(board[row][col]), curses.color_pair(4))
+      
+      cursor.set_x(1)
+
+      if col != 7:
+        cursor.set_x(3)
+    # Print Right Border
+    vertical_line(stdscr, cursor, side='R')
+  cursor.reset_x()
+  horizontal_line(stdscr, cursor)
+
+  if progress:
+    # Print The Progress Bar
+    print_progress_bar(stdscr, cursor, board)
+  
+  if initialize:
     cursor.set_x(-cursor.get_x())
     cursor.set_y(-cursor.get_y())
-    stdscr.addstr(cursor.get_y(), cursor.get_x(), 'completed: ')
-    stdscr.addstr(cursor.get_y(), cursor.get_x() + 11, ' ' * cursor.max_x)
-    stdscr.addstr(cursor.get_y(),
-                  cursor.get_x() + 11, get_progress(board),
-                  curses.color_pair(5))
-
-
-def print_board(stdscr,
-                cursor: Cursor,
-                board: List[List[int]],
-                progress: bool = False,
-                sleep_value: float = 0.5,
-                initialize: bool = False) -> None:
-    '''
-    paint the board on the window
-    '''
-    horizontal_line(stdscr, cursor)
-    for row in range(8):
-        cursor.reset_x()
-
-        # print empty line
-        if 0 < row <= 7:
-            stdscr.addstr(cursor.get_y(), cursor.get_x(), '|' + ' ' * 29 + '|',
-                          curses.color_pair(1))
-            cursor.set_y(1)
-
-        # print left border
-        vertical_line(stdscr, cursor, side='L')
-
-        for col in range(8):
-            # cell is visited
-            if board[row][col] == 1:
-                stdscr.addstr(cursor.get_y(), cursor.get_x(),
-                              str(board[row][col]), curses.color_pair(2))
-            # knight's cell
-            elif board[row][col] == 2:
-                stdscr.addstr(cursor.get_y(), cursor.get_x(),
-                              str(board[row][col]), curses.color_pair(3))
-            # cell is unvisited
-            else:
-                stdscr.addstr(cursor.get_y(), cursor.get_x(),
-                              str(board[row][col]), curses.color_pair(4))
-            cursor.set_x(1)
-
-            if col != 7:
-                cursor.set_x(3)
-
-        # print right border
-        vertical_line(stdscr, cursor, side='R')
-
-    cursor.reset_x()
-    horizontal_line(stdscr, cursor)
-
-    if progress:
-        # print the progress bar
-        print_progress_bar(stdscr, cursor, board)
-    
-    if initialize:
-        cursor.set_x(-cursor.get_x())
-        cursor.set_y(-cursor.get_y())
-        stdscr.addstr(cursor.get_y(), cursor.get_x(), 'initializing......', curses.color_pair(2))
-
-    stdscr.refresh()
-    sleep(sleep_value)
+    stdscr.addstr(cursor.get_y(), cursor.get_x(), 'initializing......', curses.color_pair(2))
+  
+  stdscr.refresh()
+  sleep(sleep_value)
