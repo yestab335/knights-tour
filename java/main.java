19edcd285136e087dad33e6cdf5f2d9d47988168
Adam <yestab335@gmail.com>
2024-08-18 17:46:28 -0400

    Initial commit

--- java/main.java
+++ java/main.java
@@ -1,2 +1,14 @@
-// Chess Board
-import board.java;
+/**
+ * Main
+*/
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    ChessBoard board = new ChessBoard();
+    board.printBoard();
+  }
+}
